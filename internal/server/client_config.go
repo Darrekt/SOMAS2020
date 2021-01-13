a3084f4a1f8599eb77430d26430160ef8c12f571
Michalis Pardalos <m.pardalos@gmail.com>
2021-01-14 00:35:28 +0200

    Add configs for all team1 foraging methods

--- internal/server/client_config.go
+++ internal/server/client_config.go
@@ -10,11 +10,33 @@ type ClientFactory func(shared.ClientID) baseclient.Client
 
 func DefaultClientConfig() map[shared.ClientID]ClientFactory {
 	return map[shared.ClientID]ClientFactory{
+		shared.Team1: team1.DefaultClient,
+		shared.Team2: team1.DefaultClient,
+		shared.Team3: team1.DefaultClient,
+		shared.Team4: team1.DefaultClient,
+		shared.Team5: team1.DefaultClient,
+		shared.Team6: team1.DefaultClient,
+	}
+}
+
+func ROIClientConfig() map[shared.ClientID]ClientFactory {
+	return map[shared.ClientID]ClientFactory{
 		shared.Team1: team1.ROIClient,
 		shared.Team2: team1.ROIClient,
+		shared.Team3: team1.ROIClient,
+		shared.Team4: team1.ROIClient,
+		shared.Team5: team1.ROIClient,
+		shared.Team6: team1.ROIClient,
+	}
+}
+
+func RegressionClientConfig() map[shared.ClientID]ClientFactory {
+	return map[shared.ClientID]ClientFactory{
+		shared.Team1: team1.RegressionClient,
+		shared.Team2: team1.RegressionClient,
 		shared.Team3: team1.RegressionClient,
 		shared.Team4: team1.RegressionClient,
-		shared.Team5: team1.FlipClient,
-		shared.Team6: team1.FlipClient,
+		shared.Team5: team1.RegressionClient,
+		shared.Team6: team1.RegressionClient,
 	}
 }
